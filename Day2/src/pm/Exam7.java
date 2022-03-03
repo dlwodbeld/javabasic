d1752f78f7ffd3000365e5825e78a32eaa899398
dlwodbeld <98381537+dlwodbeld@users.noreply.github.com>
2022-03-03 18:11:33 +0900

    Day2

--- Day2/src/pm/Exam7.java
+++ Day2/src/pm/Exam7.java
@@ -14,15 +14,19 @@ public class Exam7 {
 //		System.out.println(i);
 //		
 //		
+
 		
-//		for(int i = 0; i <5 ; i++) {
-//			for(int j = 0; j < 5 ;j++) {
+//직각삼각형 만들기	
+//		1.
+//		for(int i = 0; i <6 ; i++) {
+//			for(int j = 0; j < 6 ;j++) {
 //				System.out.print("*");
 //			}
 //			System.out.println();
 //		}
-//		for(int i = 6; i >1 ; i--) {
-//			for(int j = 6; j > i ;j--) {
+//		2.
+//		for(int i = 6; i >=0 ; i--) {
+//			for(int j = 0; j <= i ;j++) {
 //
 //				System.out.print("*");
 //			}
@@ -31,29 +35,41 @@ public class Exam7 {
 //				break;
 //			}
 //		}
+//		
 		
-		int scores[] = {70,60,55,75,95,90,80,80,85,100};
-		int sum = 0;
-		int max = 0;
-		int min = scores[0];
-		int avg = 0;
+//정삼각형 만들기		
+//		for (int i=0;i<6;i++) {
+//			for(int j=0;j<6-i;j++) {
+//				System.out.print(" ");
+//				}
+//			for(int n=0;n<i*2+1;n++) {
+//				System.out.print("*");
+//				}
+//			System.out.println();
+//		    }
 		
-		for (int score:scores) {
-			sum += score;
-			avg = sum / scores.length;
-			if(score > max) {
-				max = score;
-			}
-			else if(score < min) {
-				min = score;
-			}
-
-		}
-		System.out.println("합계 : "+sum);
-		System.out.println("평균 : "+avg);
-		System.out.println("최고점수 : "+max);
-		System.out.println("최저점수 : "+min);
-		
+//		int scores[] = {70,60,55,75,95,90,80,80,85,100};
+//		int sum = 0;
+//		int max = 0;
+//		int min = scores[0];
+//		int avg = 0;
+//		
+//		for (int score:scores) {
+//			sum += score;
+//			avg = sum / scores.length;
+//			if(score > max) {
+//				max = score;
+//			}
+//			else if(score < min) {
+//				min = score;
+//			}
+//
+//		}
+//		System.out.println("합계 : "+sum);
+//		System.out.println("평균 : "+avg);
+//		System.out.println("최고점수 : "+max);
+//		System.out.println("최저점수 : "+min);
+//		
 		
 		
 		
