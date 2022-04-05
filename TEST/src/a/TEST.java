7f6cc0589a2955f9ac770f3b27ad6d6b0658646e
dlwodbeld <98381537+dlwodbeld@users.noreply.github.com>
2022-04-05 17:39:40 +0900

    Day26

---
+++ TEST/src/a/TEST.java
@@ -0,0 +1,17 @@
+package a;
+
+public class TEST {
+	public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+
+        int reverse=0;
+        int temp=x;
+        while(temp!=0){
+            reverse = reverse*10 + temp%10;
+            temp /= 10;
+        }
+        return reverse == x;
+    }
+}
